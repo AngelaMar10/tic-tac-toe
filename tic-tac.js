db6b5f9d1ace35bcf37c45de63f69353316d0117
angelama <fotografahz@gmail.com>
2022-11-10 14:56:11 +1030

    game stopping after a winner

--- tic-tac.js
+++ tic-tac.js
@@ -13,15 +13,14 @@ for (let index = 0; index < boxes.length; index++) {
     
 }
 // the function asigns the movement to the current player.
+let gameActive = true
 function makeMovement(event) {
     let box = event.target
-    if (!box.textContent) {
+    if (!box.textContent && gameActive) {
 // ! if there is anything inside
         setCurrentPlayer ()
         box.textContent = currentPlayer;
         winningCheck ()
-        
-
     } 
 }
 // It checks if the player is the number 1, changes to number 2 and so on.
@@ -47,23 +46,30 @@ var box7 = (document.getElementById('7'));
 var box8 = (document.getElementById('8'));
 var box9 = (document.getElementById('9'));
 
+
 if ((box1.textContent === 'X' && box2.textContent === 'X' && box3.textContent === 'X') || (box4.textContent === 'X' && box5.textContent === 'X' && box6.textContent === 'X') || (box7.textContent === 'X' && box8.textContent === 'X' && box9.textContent === 'X')){
     document.querySelector('.Text').textContent = 'Player 1' 
-}
+    gameActive = false;
+} 
 else if ((box1.textContent === 'X' && box4.textContent === 'X' && box7.textContent === 'X') || (box2.textContent === 'X' && box5.textContent === 'X' && box8.textContent === 'X') || (box3.textContent === 'X' && box6.textContent === 'X' && box9.textContent === 'X')){
     document.querySelector('.Text').textContent = 'Player 1' 
+    gameActive = false;
 }
 else if ((box1.textContent === 'X' && box5.textContent === 'X' && box9.textContent === 'X') || (box3.textContent === 'X' && box5.textContent === 'X' && box7.textContent === 'X')){
     document.querySelector('.Text').textContent = 'Player 1' 
+    gameActive = false;
 }
 else if ((box1.textContent === 'O' && box2.textContent === 'O' && box3.textContent === 'O') || (box4.textContent === 'O' && box5.textContent === 'O' && box6.textContent === 'O') || (box7.textContent === 'O' && box8.textContent === 'O' && box9.textContent === 'O')){
     document.querySelector('.Text').textContent = 'Player 2' 
+    gameActive = false;
 }
 else if ((box1.textContent === 'O' && box4.textContent === 'O' && box7.textContent === 'O') || (box2.textContent === 'O' && box5.textContent === 'O' && box8.textContent === 'O') || (box3.textContent === 'O' && box6.textContent === 'O' && box9.textContent === 'O')){
     document.querySelector('.Text').textContent = 'Player 2' 
+    gameActive = false;
 }
 else if ((box1.textContent === 'O' && box5.textContent === 'O' && box9.textContent === 'O') || (box3.textContent === 'O' && box5.textContent === 'O' && box7.textContent === 'O')){
     document.querySelector('.Text').textContent = 'Player 2' 
+    gameActive = false;
 }
 else if ((box1.textContent=== 'X' || box1.textContent=== 'O') && (box2.textContent=== 'X' || box2.textContent=== 'O') && (box3.textContent=== 'X' || box3.textContent=== 'O') && (box4.textContent=== 'X' || box4.textContent=== 'O') && (box5.textContent=== 'X' || box5.textContent=== 'O') && (box6.textContent=== 'X' || box6.textContent=== 'O') &&(box7.textContent=== 'X' || box7.textContent=== 'O') && (box8.textContent=== 'X' || box8.textContent=== 'O') &&(box9.textContent=== 'X' || box9.textContent=== 'O')){
     document.querySelector('.Text').textContent = 'Draw Match'
@@ -72,6 +78,8 @@ else if ((box1.textContent=== 'X' || box1.textContent=== 'O') && (box2.textConte
 else {
     document.querySelector('.Text').textContent = 'Keep playing'
 }
+
+
 }
 
 document.querySelector('.play-again').addEventListener('click', resetGame);
@@ -89,23 +97,4 @@ function resetGame() {
  document.getElementById('9').value = '';
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-  
 
